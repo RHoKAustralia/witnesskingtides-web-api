45eead331542267061e401325ca9b5e2a8f582f6
Peter Berry <pwcberry@gmail.com>
2013-12-08 10:46:58 +1100

    Added King Tides api endpoint

---
+++ WitnessKingTides.Web.Api/Controllers/KingTidesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WitnessKingTides.Web.Api.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WitnessKingTides.Web.Api.Controllers
+{
+    public class KingTidesController : ApiController
+    {
+        public IEnumerable<KingTideModel> Get()
+        {
+            var dataDirectory = AppDomain.CurrentDomain.GetData("DataDirectory").ToString();
+
+            IEnumerable<KingTideModel> kingTides = null;
+
+            using (var reader = File.OpenText(Path.Combine(dataDirectory, "places-king-tides.json")))
+            {
+                var json = reader.ReadToEnd();
+
+                kingTides = JsonConvert.DeserializeObject<KingTideModel[]>(json);
+            }
+
+            return kingTides;
+        }
+    }
+}
